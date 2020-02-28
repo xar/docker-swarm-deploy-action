4b306ac5521faf4c797cdb44ffe172b478c8ba16
Samuel <samo@vrablik.eu>
2020-02-28 13:54:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,10 +25,10 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
 
     # Save private key to a file and register it with the agent.
     mkdir -p "$HOME/.ssh"
-    printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/id_rsa"
-    chmod 600 "$HOME/.ssh/id_rsa"
+    printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
+    chmod 600 "$HOME/.ssh/docker"
     eval $(ssh-agent)
-    ssh-add "$HOME/.ssh/id_rsa"
+    ssh-add "$HOME/.ssh/docker"
     
     # Add public key to known hosts.
     echo "Add public key verify..."
